cb1b86b0d2b9c9f004b709dae162cf462d384ed5
qirall79 <belfatmi79@gmail.com>
2023-02-04 17:39:34 +0100

    Handle friend requests

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -35,3 +35,113 @@ exports.user_get = async (req, res, next) => {
     return next(err);
   }
 };
+
+// Handle friend request
+exports.friend_post = async (req, res, next) => {
+  try {
+    const userId = req.body.user;
+    const { receiverId } = req.params;
+    const [user, receiver] = await Promise.all([
+      User.findById(userId),
+      User.findById(receiverId),
+    ]);
+
+    if (receiver.receivedRequests.includes(userId)) {
+      const userRequests = user.sentRequests.filter(
+        (id) => JSON.stringify(id) !== JSON.stringify(receiver._id)
+      );
+      const receiverRequests = receiver.receivedRequests.filter(
+        (id) => JSON.stringify(id) !== JSON.stringify(user._id)
+      );
+      await Promise.all([
+        User.findByIdAndUpdate(receiverId, {
+          receivedRequests: receiverRequests,
+        }),
+        User.findByIdAndUpdate(userId, { sentRequests: userRequests }),
+      ]);
+      return res.json({ message: "Request is canceled successfully." });
+    }
+
+    await Promise.all([
+      User.findByIdAndUpdate(receiver._id, {
+        receivedRequests: [...receiver.receivedRequests, user._id],
+      }),
+      User.findByIdAndUpdate(user._id, {
+        sentRequests: [...user.sentRequests, receiver._id],
+      }),
+    ]);
+    res.json({
+      message: `Request sent to ${
+        receiver.firstName + " " + receiver.lastName
+      } successfully.`,
+    });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Handle friend request response (accept/decline)
+exports.friend_update = async (req, res, next) => {
+  try {
+    const { senderId } = req.params;
+    const userId = req.body.user;
+    const action = req.body.action;
+
+    const [user, sender] = await Promise.all([
+      User.findById(userId),
+      User.findById(senderId),
+    ]);
+
+    const senderRequests = sender.sentRequests.filter(
+      (id) => JSON.stringify(id) !== JSON.stringify(user._id)
+    );
+    const userRequests = user.receivedRequests.filter(
+      (id) => JSON.stringify(id) !== JSON.stringify(sender._id)
+    );
+
+    await Promise.all([
+      User.findByIdAndUpdate(userId, { receivedRequests: userRequests }),
+      User.findByIdAndUpdate(senderId, { sentRequests: senderRequests }),
+    ]);
+
+    if (action === "decline") {
+      return res.json({ message: "Request declined successfully." });
+    }
+    await Promise.all([
+      User.findByIdAndUpdate(userId, { friends: [...user.friends, senderId] }),
+      User.findByIdAndUpdate(senderId, {
+        friends: [...sender.friends, userId],
+      }),
+    ]);
+    return res.json({ message: "Request accepted successfully." });
+  } catch (err) {
+    return next(err);
+  }
+};
+
+// Delete friend
+exports.friend_delete = async (req, res, next) => {
+  try {
+    const userId = req.body.user;
+    const { friendId } = req.params;
+    const [user, friend] = await Promise.all([
+      User.findById(userId),
+      User.findById(friendId),
+    ]);
+    const userFriends = user.friends.filter(
+      (id) => JSON.stringify(id) !== JSON.stringify(friend._id)
+    );
+    const friendFriends = friend.friends.filter(
+      (id) => JSON.stringify(id) !== JSON.stringify(user._id)
+    );
+
+    await Promise.all([
+      User.findByIdAndUpdate(user._id, { friends: userFriends }),
+      User.findByIdAndUpdate(friend._id, { friends: friendFriends }),
+    ]);
+
+    return res.json({ message: "Friend removed successfully." });
+  } catch (err) {
+    return next(err);
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -3,15 +3,39 @@ const router = express.Router();
 const passport = require("passport");
 const usersController = require("../controllers/usersController");
 
+// Get current user
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
   usersController.currentUser_get
 );
+
+// Get specific user
 router.get(
   "/:id",
   passport.authenticate("jwt", { session: false }),
   usersController.user_get
+);
+
+// Handle friend request
+router.post(
+  "/:receiverId",
+  passport.authenticate("jwt", { session: false }),
+  usersController.friend_post
+);
+
+// Handle response to friend request
+router.put(
+  "/:senderId",
+  passport.authenticate("jwt", { session: false }),
+  usersController.friend_update
+);
+
+// Delete friend
+router.delete(
+  "/:friendId",
+  passport.authenticate("jwt", { session: false }),
+  usersController.friend_delete
 );
 
 module.exports = router;
