7f1c3f0db416c868881376d77d789a7166c84d70
qirall79 <belfatmi79@gmail.com>
2023-02-10 17:24:59 +0100

    Fix env variables errors

--- service/uploadService.js
+++ service/uploadService.js
@@ -2,9 +2,10 @@ const multer = require("multer");
 const cloudinary = require("cloudinary");
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "docpycf9f",
+  api_key: process.env.CLOUDINARY_API_KEY || "422224551253893",
+  api_secret:
+    process.env.CLOUDINARY_API_SECRET || "sQ5Lfkoq-YOVPRu6J-Z7ls5eM7E",
 });
 
 const memoryStorage = multer.memoryStorage();
